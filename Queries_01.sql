1536e7f9c221bbfc18e6b9c69441f4de0c779bcd
Daniel Friedman <51696910+danielskih@users.noreply.github.com>
2022-04-21 12:16:48 +0200

    Update Queries_01.sql

--- Queries_01.sql
+++ Queries_01.sql
@@ -1,6 +1,6 @@
 use olist;
 # Top 10 dealers.
-select ROUND(sum(price)), seller_id from order_items GROUP BY seller_id ORDER BY sum(price) DESC LIMIT 10;
+select ROUND(sum(price)), seller_id from order_items GROUP BY seller_id ORDER BY sum(price) DESC;
 # Total revenue per item.
 select ROUND(sum(price)) AS total_revenue_item, product_id from order_items GROUP BY product_id ORDER BY total_revenue_item DESC;
 # Total customer over time.
@@ -19,7 +19,9 @@ FROM order_items
 INNER JOIN orders using (order_id)
 GROUP BY monat, years
 ORDER BY years;
+
 #Total revenue over time.
+USE olist_sql;
 SELECT ROUND(SUM(payment_value)) AS revenue,
 MONTH(order_purchase_timestamp) AS monat,
 YEAR(order_purchase_timestamp) AS jahr
@@ -30,5 +32,16 @@ GROUP BY monat, jahr
 ORDER BY jahr, monat;
 #Salers marketing leads origin.
 SELECT count(mql_id), origin FROM olist_marketing_qualified_leads_dataset WHERE origin IS NOT NULL group by origin;
+#Orders status
 USE olist_sql;
 SELECT COUNT(order_id), order_status FROM olist_sql.olist_orders_dataset GROUP BY order_status;
+# Revenue per marketing channel
+USE olist_sql;
+select ROUND(SUM(payment_value)) AS revenue,origin   FROM olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+INNER JOIN olist_closed_deals_dataset USING(seller_id)
+LEFT JOIN olist_marketing_qualified_leads_dataset USING(mql_id)
+GROUP BY origin ORDER BY revenue DESC;
+
+
+
