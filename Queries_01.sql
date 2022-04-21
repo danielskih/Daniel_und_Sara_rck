e1e2fb1e1d52086f25fbc88f56d59207a2ffcee3
Daniel Friedman <51696910+danielskih@users.noreply.github.com>
2022-04-21 19:23:41 +0200

    Added new csv files. Update of DrillDown tableau file.

--- Queries_01.sql
+++ Queries_01.sql
@@ -1,8 +1,20 @@
-use olist;
+use olist_sql;
+#Average revenue per user
+select avg(revenue) from 
+(select sum(payment_value) as revenue from olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+GROUP BY seller_id ORDER BY revenue DESC) as revenue;
 # Top 10 dealers.
-select ROUND(sum(price)), seller_id from order_items GROUP BY seller_id ORDER BY sum(price) DESC;
-# Total revenue per item.
-select ROUND(sum(price)) AS total_revenue_item, product_id from order_items GROUP BY product_id ORDER BY total_revenue_item DESC;
+select ROUND(sum(payment_value)) as revenue, seller_id from olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+GROUP BY seller_id ORDER BY revenue DESC;
+# Total revenue per category.
+select ROUND(sum(payment_value)) AS total_revenue_item, product_category_name_english AS product  from olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+INNER JOIN olist_products_dataset USING(product_id)
+INNER JOIN product_category_name_translation 
+ON ï»¿product_category_name=product_category_name
+GROUP BY product ORDER BY total_revenue_item DESC;
 # Total customer over time.
 SELECT COUNT(distinct customer_id) AS customers, 
 YEAR(order_purchase_timestamp) as "years", 
@@ -37,11 +49,30 @@ USE olist_sql;
 SELECT COUNT(order_id), order_status FROM olist_sql.olist_orders_dataset GROUP BY order_status;
 # Revenue per marketing channel
 USE olist_sql;
-select ROUND(SUM(payment_value)) AS revenue,origin   FROM olist_order_items_dataset 
+select ROUND(SUM(payment_value)) AS revenue, origin   FROM olist_order_items_dataset 
 INNER JOIN olist_order_payments_dataset USING(order_id)
 INNER JOIN olist_closed_deals_dataset USING(seller_id)
 LEFT JOIN olist_marketing_qualified_leads_dataset USING(mql_id)
 GROUP BY origin ORDER BY revenue DESC;
+# Average ratings 
+SELECT round(AVG(review_score)), YEAR(review_creation_date) AS jahr, MONTH(review_creation_date) AS monat FROM olist_order_reviews_dataset GROUP BY jahr, monat Order by jahr, monat;
 
+SELECT COUNT(review_score) as "count", review_score AS score, YEAR(review_creation_date) AS jahr, QUARTER(review_creation_date)
+AS Q FROM olist_order_reviews_dataset GROUP BY 'COUNT', score, jahr, Q ORDER BY jahr, Q, score;
 
+# Sellers
+SELECT COUNT(seller_id) FROM olist_sql.olist_sellers_dataset;
 
+USE olist_sql;
+SELECT ROUND(AVG(temp.revenue)), temp.origin FROM
+(select ROUND(SUM(payment_value)) AS revenue, origin  FROM olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+INNER JOIN olist_closed_deals_dataset USING(seller_id)
+LEFT JOIN olist_marketing_qualified_leads_dataset USING(mql_id)
+GROUP BY seller_id, origin ORDER BY revenue DESC) AS temp
+GROUP BY temp.origin;
+select ROUND(SUM(payment_value)) AS revenue, origin, SELLER_ID   FROM olist_order_items_dataset 
+INNER JOIN olist_order_payments_dataset USING(order_id)
+INNER JOIN olist_closed_deals_dataset USING(seller_id)
+LEFT JOIN olist_marketing_qualified_leads_dataset USING(mql_id)
+GROUP BY seller_id, origin ORDER BY revenue DESC;
--- top_10_products.sql
+++ top_10_products.sql
@@ -1,9 +1,10 @@
 use olist_sql;
 #Get top 12 grossing products.
-SELECT  COUNT(items.product_id), products.product_category_name  FROM olist_order_items_dataset AS items
+SELECT  COUNT(items.product_id), product_category_name_english  FROM olist_order_items_dataset AS items
 INNER JOIN olist_products_dataset AS products USING(product_id)
-GROUP BY products.product_category_name
-ORDER BY COUNT(items.product_id)  DESC LIMIT 12;
+INNER JOIN product_category_name_translation ON product_category_name = ï»¿product_category_name
+GROUP BY product_category_name_english
+ORDER BY COUNT(items.product_id)  DESC;
 
 # Get bottom 19 grossing products.
 SELECT ROUND(SUM(price)) AS gross_revenue, product_category_name FROM olist_order_items_dataset AS items 
